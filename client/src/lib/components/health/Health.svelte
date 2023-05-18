c8bac7bf7bc5286c071c655db211b8517533761e
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-19 01:04:55 +0200

    refactor: improve styling

--- client/src/lib/components/health/Health.svelte
+++ client/src/lib/components/health/Health.svelte
@@ -3,17 +3,21 @@
     import { health } from '../../stores/health.store';
     import Text from '../UI/Text.svelte';
 
-    $: color = $health?.ok ? 'rgba(76, 175, 80, 0.7)' : 'rgba(244, 67, 54, 1)';
+    $: color = $health?.ok ? 'var(--ui-color-semantic-success)' : 'var(--ui-color-semantic-error)';
 </script>
 
-<div class="layout flex-row flex-align-center small">
+<div class="layout flex-row flex-align-center small container">
     <div class="indicator" style="border-color: {color}; " />
-    <Text size="small">
+    <Text size="small" spacing="tight">
         Version: {$health?.version || '--'}
     </Text>
 </div>
 
 <style lang="scss">
+    .container {
+        padding: 0.4rem;
+    }
+
     .indicator {
         width: 1.2rem;
         font-size: 1.2rem;
