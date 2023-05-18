94b555ba85944b8d3ea76eba52bd2f4363f9df67
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-19 01:03:23 +0200

    feat: add spacing prop to text component

--- client/src/lib/components/UI/Text.svelte
+++ client/src/lib/components/UI/Text.svelte
@@ -1,13 +1,28 @@
 <!-- @format -->
 <script lang="ts">
+    import { joinClassNames } from '../../../utils/joinClassnames';
+
     export let size: 'small' | 'regular' | 'large' = 'regular';
+    export let spacing: 'relaxed' | 'normal' | 'tight' = 'normal';
 </script>
 
-<span class={size}>
+<span class={joinClassNames(size, spacing)}>
     <slot />
 </span>
 
 <style>
+    .relaxed {
+        line-height: 2rem;
+    }
+
+    .normal {
+        line-height: 1.6rem;
+    }
+
+    .tight {
+        line-height: 1.2rem;
+    }
+
     .small {
         font-size: 1.2rem;
     }
