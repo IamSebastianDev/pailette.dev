a4d5090f0652455fa519010140e5dac25dde6677
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-19 00:50:10 +0200

    feat: create basic text component

---
+++ client/src/lib/components/UI/Text.svelte
@@ -0,0 +1,22 @@
+<!-- @format -->
+<script lang="ts">
+    export let size: 'small' | 'regular' | 'large' = 'regular';
+</script>
+
+<span class={size}>
+    <slot />
+</span>
+
+<style>
+    .small {
+        font-size: 1.2rem;
+    }
+
+    .regular {
+        font-size: 1.6rem;
+    }
+
+    .large {
+        font-size: 2.1rem;
+    }
+</style>
