faed2e4cbadc259f33d8da25270624ff8d884245
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-21 16:28:38 +0200

    feat: create basic paragraph component

---
+++ client/src/lib/components/UI/Text/Paragraph.svelte
@@ -0,0 +1,14 @@
+<!-- @format -->
+<script lang="ts">
+    import { joinClassNames } from '../../../../utils/joinClassNames';
+    /**
+     * @type {string[]}
+     * @description
+     * Additional classnames to add.
+     */
+    export let classes: string[] = [];
+</script>
+
+<p class={joinClassNames(...classes)}>
+    <slot />
+</p>
