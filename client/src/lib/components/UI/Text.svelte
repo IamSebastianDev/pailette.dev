35bc00987b75078348343fd851f0b62c69505302
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-21 16:18:03 +0200

    feat: add additional properties to text component

--- client/src/lib/components/UI/Text.svelte
+++ client/src/lib/components/UI/Text.svelte
@@ -2,29 +2,71 @@
 <script lang="ts">
     import { joinClassNames } from '../../../utils';
 
+    /**
+     * @type { 'small' | 'regular' }
+     * @description
+     * Font-size of the text component.
+     */
     export let size: 'small' | 'regular' = 'regular';
+
+    /**
+     * @type { 'relaxed' | 'tight' }
+     * @description
+     * Line-height of the text component.
+     */
     export let spacing: 'relaxed' | 'tight' = 'relaxed';
+
+    /**
+     * @type { 'wide' | 'normal' }
+     * @description
+     * Tracking of the text component.
+     */
+    export let tracking: 'wide' | 'normal' = 'normal';
+
+    /**
+     * @type { boolean }
+     * @description
+     * Boolean flag indicating if the text should be bold.
+     */
+    export let bold: boolean = false;
+
     export let classes: string[] = [];
 </script>
 
-<span class={joinClassNames(size, spacing, ...classes)}>
+<span class={joinClassNames('text', bold ? 'bold' : null, size, spacing, tracking, ...classes)}>
     <slot />
 </span>
 
-<style>
-    .relaxed {
-        line-height: 1.8rem;
-    }
+<style lang="scss">
+    .text {
+        font-family: inherit;
 
-    .tight {
-        line-height: 1.2rem;
-    }
+        &.bold {
+            font-weight: 500;
+        }
 
-    .small {
-        font-size: 1.2rem;
-    }
+        &.wide {
+            letter-spacing: 0.1rem;
+        }
 
-    .regular {
-        font-size: 1.6rem;
+        &.normal {
+            letter-spacing: inherit;
+        }
+
+        &.relaxed {
+            line-height: 1.8rem;
+        }
+
+        &.tight {
+            line-height: 1.2rem;
+        }
+
+        &.small {
+            font-size: 1.2rem;
+        }
+
+        &.regular {
+            font-size: 1.5rem;
+        }
     }
 </style>
