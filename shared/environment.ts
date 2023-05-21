e24ccff7affbd7b3ecdd19dd08e150171f442732
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-05-21 14:27:15 +0200

    refactor: improve environment assertion

--- shared/environment.ts
+++ shared/environment.ts
@@ -1,13 +1,22 @@
 /** @format */
 
+import { assertEnvironment } from './utils/assertEnvironment.util';
+export type Environment = 'development' | 'production';
 export const environment = {
-    get current() {
-        return process.env.NODE_ENV;
-    },
-    isDevelopment: () => {
-        return process.env.NODE_ENV !== 'production';
-    },
-    isProduction: () => {
-        return process.env.NODE_ENV === 'production';
+    api_base_url: 'http://localhost:3000/api/v1',
+    get current(): 'development' | 'production' {
+        let environment: Environment = 'development';
+
+        if (assertEnvironment()) {
+            if (window && (window as any).environment) {
+                environment = (window as any).environment as Environment;
+            }
+        } else {
+            if (process.env.NODE_ENV) {
+                environment = process.env.NODE_ENV as Environment;
+            }
+        }
+
+        return environment;
     },
 };
