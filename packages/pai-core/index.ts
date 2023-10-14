1cc421f354aaac164a05dbfb3648be4d8d3c1460
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-14 10:35:32 +0200

    refactor: move http service to core library

--- packages/pai-core/index.ts
+++ packages/pai-core/index.ts
@@ -5,3 +5,4 @@ export type { _Meta } from './lib/types/_Meta';
 export type { PrimitiveType } from './lib/types/PrimitiveType';
 export { PrimitiveParser } from './lib/utils/PrimitiveParser';
 export { isNotEmpty } from './lib/utils/isNotEmpty';
+export { httpService } from './lib/services/httpService/httpService';
