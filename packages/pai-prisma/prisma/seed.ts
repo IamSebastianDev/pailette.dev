789f834a706cbec4d46f41b65bab6b894699bd04
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-18 17:56:44 +0200

    chore: add user to seeder

--- packages/pai-prisma/prisma/seed.ts
+++ packages/pai-prisma/prisma/seed.ts
@@ -1,4 +1,13 @@
 /** @format */
 
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 const client = new PrismaClient();
+
+await client.user.create({
+    data: {
+        name: 'Super User',
+        email: 'super.user@test.dev',
+        password: bcrypt.hashSync('secret', 12),
+    },
+});
