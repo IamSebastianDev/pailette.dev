6226cb6189f99317eaf88171fa1baef739557560
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-18 18:04:21 +0200

    fix: correct imports

--- apps/pailette/src/lib/services/httpService/httpService.ts
+++ apps/pailette/src/lib/services/httpService/httpService.ts
@@ -1,7 +1,7 @@
 /** @format */
-import { httpService } from '@pai/core';
+import { createHttpService } from '@pai/core';
 
-export const apiService = httpService({ baseUrl: 'http://localhost:3000/api/v1' });
+export const apiService = createHttpService({ baseUrl: 'http://localhost:3000/api/v1' });
 apiService.request.use((req) => {
     return req;
 });
