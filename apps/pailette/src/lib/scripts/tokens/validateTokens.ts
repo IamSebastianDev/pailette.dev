d0ca88a3227a34653cd2da53c8e45d643f39f56b
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-21 01:59:07 +0200

    refactor: removed dead code

--- apps/pailette/src/lib/scripts/tokens/validateTokens.ts
+++ apps/pailette/src/lib/scripts/tokens/validateTokens.ts
@@ -9,12 +9,7 @@ export const validateTokens = async (token: string): Promise<boolean> => {
     const secret = new TextEncoder().encode(environment.api_bearer_secret);
 
     try {
-        const { payload } = await jose.jwtVerify(token, secret);
-
-        // Return false if the token is invalid or expired
-        if (!payload || (payload && payload.exp && payload.exp <= now)) {
-            return false;
-        }
+        await jose.jwtVerify(token, secret);
 
         return true;
     } catch (error) {
