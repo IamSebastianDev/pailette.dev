c4dcef4fa3e031b43185b5785d15e7df0f305c7a
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-21 00:23:44 +0200

    feat: create environment file

---
+++ apps/pailette/src/environment.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export const environment = {
+    api_bearer_secret: 'secret',
+    api_base_url: 'http://localhost:3000/api/v1',
+};
--- apps/pailette/src/lib/services/http/http.service.ts
+++ apps/pailette/src/lib/services/http/http.service.ts
@@ -2,7 +2,8 @@
 import { createHttpService } from '@pai/core';
 import { authInterceptor } from './middlewares/auth.interceptor';
 import { customHeaderSet } from './middlewares/customHeaders.interceptor';
+import { environment } from '../../../environment';
 
-export const httpService = createHttpService({ baseUrl: 'http://localhost:3000/api/v1' });
+export const httpService = createHttpService({ baseUrl: environment.api_base_url });
 httpService.request.use(authInterceptor);
 httpService.request.use(customHeaderSet);
