f5875ec82d6707157808917c7689c7d3ff3c5edb
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-10-21 01:59:36 +0200

    chore: add request logger

--- apps/pailette/src/lib/services/http/http.service.ts
+++ apps/pailette/src/lib/services/http/http.service.ts
@@ -3,7 +3,9 @@ import { createHttpService } from '@pai/core';
 import { authInterceptor } from './middlewares/auth.interceptor';
 import { customHeaderSet } from './middlewares/customHeaders.interceptor';
 import { environment } from '../../../environment';
+import { requestLogger } from './middlewares/requestLogger.interceptor';
 
 export const httpService = createHttpService({ baseUrl: environment.api_base_url });
 httpService.request.use(authInterceptor);
 httpService.request.use(customHeaderSet);
+httpService.request.use(requestLogger);
